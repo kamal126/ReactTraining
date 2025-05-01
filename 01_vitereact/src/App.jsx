1bfb84c7de29b53b3cadde0c3518df95343dd70e
kamal126 <kamalchanda265@gmail.com>
2025-05-01 21:52:48 +0530

    Custom react app using Vite

---
+++ 01_vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Name from './Name'
+function App() {
+  const name = "Kamal Chandra"
+  return (
+    <>
+      <Name/>
+      <h3>My name is: {name}</h3>
+    </>
+  )
+}
+
+export default App
---
+++ 01_vitereact/src/Name.jsx
@@ -0,0 +1,8 @@
+
+function Name() {
+    return(
+        <h3>Hey my name is: </h3>
+    )
+}
+
+export default Name;
